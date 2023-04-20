9a4219099f0ee7c28396574f9bac53784466ff8e
AndreyPotapovAndrey <125595526+AndreyPotapovAndrey@users.noreply.github.com>
2023-04-20 23:08:37 +0300

    Update ya.py

--- ya.py
+++ ya.py
@@ -5,6 +5,7 @@ from alive_progress import alive_bar
 import time
 
 
+
 class YaUploader:
     def __init__(self, token):
         self.token = token
@@ -30,10 +31,12 @@ class YaUploader:
         head = self.get_headers()
         params = {'path': f'disk:/{folder_name}'}
 
-        requests.put(url, params=params, headers=head)
-        print('Новая папка создана')
+        response = requests.put(url, params=params, headers=head)
+        if response.status_code in [201, 409]:
+            print('Новая папка создана')
+            return folder_name
+        print('Folder_error', response.json()['error']['error_msg'])
 
-        return folder_name
 
     # def create_folder(path):
     #     """Создание папки. \n path: Путь к создаваемой папке."""
@@ -59,23 +62,19 @@ class YaUploader:
             likes = photo['likes']
             name = f'{likes}likes.jpg'
             like_list.append(likes)
-            # pprint(photo)
-            # pprint(like_list)
-            # pprint(date)
             counter = Counter(like_list)
             for v in counter.values():
                 if v > 1:
                     name = f'{likes}likes{date}.jpg'
-            # pprint(name)
-            log_dict.append({'file_name': name, 'size': photo['type']})
             path = f'{folder}/{name}'
             href = self._get_upload_link(path)
             response = requests.get(url)
             data = response.content
             resp = requests.put(href, data=data)
             if resp.status_code == 201:
-                with alive_bar(len(photos)) as bar:
-                    for _ in photos:
-                        bar()
-                        time.sleep(1)
+                    with alive_bar(len(photos)) as bar:
+                        for _ in photos:
+                            bar()
+                            time.sleep(1)
+
         return log_dict
